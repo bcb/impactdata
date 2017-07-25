eaede83e36f1409bbe4ef1a73ddaf882e6d467b1
Beau Barker <beauinmelbourne@gmail.com>
2017-07-25 14:25:58 +1000

    Adjust documentation/docstrings

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 Test project for Impact Data.
 
+A simple integration project to demonstrate your coding skills. Alert public
+transport users when their train line has disrupted services.
+
 ## Installation
 
 ```python
--- impactdata/ptv.py
+++ impactdata/ptv.py
@@ -1,9 +1,9 @@
-"""Utility functions for accessing the PTV and Slack apis"""
+"""Comms with the Public Transport Victoria API."""
 import requests
 
 from .settings import API_KEY, PTV_BASE_URL
 
 
 def get_disruptions():
-    """Get current disruptions from the PTV API"""
+    """Get current disruptions"""
     return requests.get(f'{PTV_BASE_URL}/v3/disruptions')

740f7a2c3bd6c8575212674be9ebc8b74c601330
Beau Barker <beauinmelbourne@gmail.com>
2017-07-25 13:49:48 +1000

    Add docstrings to utils module

--- impactdata/utils.py
+++ impactdata/utils.py
@@ -1,12 +1,14 @@
+"""Utility functions for accessing the PTV and Slack apis"""
 import requests
 
 from impactdata.settings import API_KEY
 
 
-
 def get_disruptions():
+    """Get current disruptions from the PTV API"""
     pass
 
 
-def alert(disruptions):
+def alert(disruptions, name, route):
+    """Alert a person of a disruption"""
     pass

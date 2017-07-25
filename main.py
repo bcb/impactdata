7c9c4d1793053f77f994954e08b3bd04c907f88c
Beau Barker <beauinmelbourne@gmail.com>
2017-07-25 18:23:56 +1000

    Refactor main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 """Alert four public transport users when their route is disrupted."""
-from impactdata import ptv, slack
+from slackclient import SlackClient
+
+from impactdata import ptv, settings
 
 
 if __name__ == '__main__':
-    # Get the current disruptions
+    # Get all disruptions
     disruptions = ptv.get_disruptions()
     # Alert the four users
-    PEOPLE = (
-        ('Barry', 'Belgrave'),
-        ('Harry', 'Hurstbridge'),
-        ('Wally', 'Werribee'),
-        ('Freddy', 'Frankston')
-    ,)
-    for name, route in PEOPLE:
-        slack.alert(name, route, disruptions)
+    slack_client = SlackClient(settings.SLACK_TOKEN)
+    for person in settings.PEOPLE:
+        # Get the list of disruptions for their route
+        route_disruptions = ptv.filter_disruptions(disruptions, person.route_id)
+        if route_disruptions:
+            # Get the disruptions into one string (saves multiple api calls)
+            descriptions = ptv.get_descriptions(disruptions)
+            # Send the alert
+            message = f'{person.route_name}: {descriptions}'
+            sc.api_call('chat.postMessage', channel=person.name, text=message)

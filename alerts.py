8489346ca1bba31032c67c93cbc45b0adb0def64
Beau Barker <beauinmelbourne@gmail.com>
2017-07-25 20:49:45 +1000

    Refactor main.py and rename to alerts.py

---
+++ alerts.py
@@ -0,0 +1,32 @@
+"""Alert public transport users when their route is disrupted."""
+from slackclient import SlackClient
+
+import ptv, settings
+
+
+def send(users):
+    # Get all disruptions
+    disruptions = ptv.get_disruptions()
+    # Get slack client
+    slack_client = SlackClient(settings.SLACK_TOKEN)
+    # Alert the users
+    for user in users:
+        # Get only the disruptions for the user's route
+        route_disruptions = ptv.filter_disruptions(disruptions, user.route_id)
+        if route_disruptions:
+            # Get the disruptions into one string (saves multiple api calls)
+            descriptions = ptv.get_descriptions(disruptions)
+            # Send the alert
+            message = f'{user.route_name} disruptions: {descriptions}'
+            slack_client.api_call(
+                'chat.postMessage', channel=user.slack_name, text=message)
+
+
+if __name__ == '__main__':
+    users = (
+        ptv.User('Barry', '@barry', 'Belgrave', 1),
+        ptv.User('Harry', '@harry', 'Hurstbridge', 2),
+        ptv.User('Wally', '@wally', 'Werribee', 3),
+        ptv.User('Freddy', '@freddy', 'Frankston', 4),
+    )
+    send(users)

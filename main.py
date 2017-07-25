1e06767d5a905797a5442ec8c430ddc963086a5e
Beau Barker <beauinmelbourne@gmail.com>
2017-07-25 14:26:09 +1000

    Refactor main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-import sys
-
+"""Alert four public transport users when their route is disrupted."""
 from impactdata import ptv, slack
 
 
 if __name__ == '__main__':
+    # Get the current disruptions
     disruptions = ptv.get_disruptions()
-    alerted = slack.alert(disruptions)
-    print(alerted, file=sys.stderr)
+    # Alert the four users
+    PEOPLE = (
+        ('Barry', 'Belgrave'),
+        ('Harry', 'Hurstbridge'),
+        ('Wally', 'Werribee'),
+        ('Freddy', 'Frankston')
+    ,)
+    for name, route in PEOPLE:
+        slack.alert(name, route, disruptions)

07cbc1362fbbf13ba869b82ff4866bf1d5c25809
Beau Barker <beauinmelbourne@gmail.com>
2017-07-26 10:35:14 +1000

    Set correct route ids in main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from ptvalerts import ptv, slack
 
 if __name__ == '__main__':
     users = (
-        ptv.User('@barry', 'Belgrave', 1),
-        ptv.User('@harry', 'Hurstbridge', 2),
-        ptv.User('@wally', 'Werribee', 3),
-        ptv.User('@freddy', 'Frankston', 4),
+        ptv.User('@barry', 'Belgrave', 2),
+        ptv.User('@harry', 'Hurstbridge', 8),
+        ptv.User('@wally', 'Werribee', 16),
+        ptv.User('@freddy', 'Frankston', 6),
     )
     disruptions = ptv.get_disruptions()
     slack.send(users, disruptions)
